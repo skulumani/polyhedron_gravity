fbdd292f671042a4e34deb52b60f8de27ad362b1
Shankar Kulumani <shanks.k@gmail.com>
2018-10-05 16:21:31 -0400

    Finish travis build

--- scripts/build_travis.sh
+++ scripts/build_travis.sh
@@ -76,7 +76,60 @@ sudo ldconfig
 ##########################################################################
 # INSTALL BOOST
 #########################################################################
+BOOST_VER=1.68.0
+BOOST_SHA256_SUM="da3411ea45622579d419bfda66f45cd0f8c32a181d84adfa936f5688388995cf"
+BOOST_URL="https://dl.bintray.com/boostorg/release/${BOOST_VER}/source/boost_1_68_0.tar.gz"
+TEMP_DIR="$(mktemp -d)"
+INSTALL_DIR="/usr/local"
+
+# This will install the latest Boost and the Boost Python library
+echo "We're going to download and install the latest Boost"
+
+echo "Installing some dependencies"
+sudo apt-get install -y build-essential libicu-dev libbz2-dev autotools-dev
+
+echo "Now downloading Boost"
+cd ${TEMP_DIR}
+mkdir boost
+wget ${BOOST_URL} -O ${TEMP_DIR}/boost.tar.gz
+# verify sha256 sum
+if ! echo "${BOOST_SHA256_SUM} boost.tar.gz" | sha256sum -c; then
+    echo "Checksum does not match. Aborting!!"
+    exit 1
+fi
+
+tar -xzf boost.tar.gz -C ./boost --strip-components=1
+
+echo "Now installing Boost and compiled libraries"
+cd boost
+./bootstrap.sh --prefix=${INSTALL_DIR} --with-libraries=all
+
+
+sudo ./b2 -j 4 install
+
+echo "Boost and are installed to $INSTALL_DIR"
 
 #########################################################################
 # INSTALL CGAL
 ########################################################################
+CGAL_VER='CGAL-4.12.1'
+TEMP_DIR="$(mktemp -d)"
+CGAL_RELEASE_URL='https://github.com/CGAL/cgal/releases/download/releases%2F'${CGAL_VER}'/'${CGAL_VER}'.tar.xz'
+
+echo "Installing some dependencies"
+sudo apt-get update
+sudo apt-get install build-essential libgmp-dev libmpfr-dev zlib1g-dev
+sudo apt-get install libgl1 libstdc++6 libgcc1 libc6 libntl-dev 
+# install CGAL for Python and build it from source for C++
+echo "Downloading ${CGAL_VER}"
+
+# download the source tarball
+cd ${TEMP_DIR}
+wget ${CGAL_RELEASE_URL} $TEMP_DIR
+tar xf ${CGAL_VER}.tar.* 
+
+echo "Installing ${CGAL_VER}"
+cd $CGAL_VER
+cmake -DWITH_examples=ON -DWITH_demos=ON -DWITH_CGAL_Qt5=OFF .
+make -j 4
+sudo make install
