722847aeab22bc72028caf466870897cc8109cb5
Shankar Kulumani <shanks.k@gmail.com>
2018-10-14 17:20:07 -0400

    Install eigen from source

--- scripts/build_travis.sh
+++ scripts/build_travis.sh
@@ -49,29 +49,29 @@ trap cleanup EXIT
 ##############################################################################
 # INSTALL EIGEN
 ###############################################################################
-# EIGEN_VER=3.3.4 INSTALL_DIR="/usr/local/include"
-# EIGEN_RELEASE_URL="https://github.com/eigenteam/eigen-git-mirror/archive/${EIGEN_VER}.tar.gz"
-# TEMP_DIR="$(mktemp -d)"
-# WORK_DIR="$(pwd)"
+EIGEN_VER=3.3.4 INSTALL_DIR="/usr/local/include"
+EIGEN_RELEASE_URL="https://github.com/eigenteam/eigen-git-mirror/archive/${EIGEN_VER}.tar.gz"
+TEMP_DIR="$(mktemp -d)"
+WORK_DIR="$(pwd)"
 
-# # download  latest release of eigen
+# download  latest release of eigen
 
-# echo "We're going to download Eigen ${EIGEN_VER} and install to ${INSTALL_DIR}"
-# cd ${TEMP_DIR}
-# mkdir ${EIGEN_VER}
-# wget ${EIGEN_RELEASE_URL} -O ${TEMP_DIR}/${EIGEN_VER}.tar.gz
-# tar -xvzf ${EIGEN_VER}.tar.gz -C ./${EIGEN_VER} --strip-components=1
+echo "We're going to download Eigen ${EIGEN_VER} and install to ${INSTALL_DIR}"
+cd ${TEMP_DIR}
+mkdir ${EIGEN_VER}
+wget ${EIGEN_RELEASE_URL} -O ${TEMP_DIR}/${EIGEN_VER}.tar.gz
+tar -xvzf ${EIGEN_VER}.tar.gz -C ./${EIGEN_VER} --strip-components=1
 
-# echo "Going to install Eigen using CMake"
-# cd ${EIGEN_VER}
-# mkdir build
-# cd build
-# cmake ..
-# sudo make install
+echo "Going to install Eigen using CMake"
+cd ${EIGEN_VER}
+mkdir build
+cd build
+cmake ..
+sudo make install
 
-# echo "Eigen installed"
+echo "Eigen installed"
 
-# sudo ldconfig
+sudo ldconfig
 
 ##########################################################################
 # INSTALL BOOST
